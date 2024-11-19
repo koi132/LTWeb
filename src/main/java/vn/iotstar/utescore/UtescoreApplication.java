582448885ed8b9dddd3ede1ce844b78bb81403c9
HieuDuong <duonghieu.7104@gmail.com>
2024-11-19 20:03:33 +0700

    init

---
+++ src/main/java/vn/iotstar/utescore/UtescoreApplication.java
@@ -0,0 +1,13 @@
+package vn.iotstar.utescore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtescoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtescoreApplication.class, args);
+	}
+
+}
